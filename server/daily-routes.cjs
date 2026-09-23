e976537b106392c296c7aadc5079cd6e32e5b1e3
hugoaguilera022 <hugoaguilera022@gmail.com>
2026-09-23 11:13:34 +0200

    Fix missing Sharp import

--- server/daily-routes.cjs
+++ server/daily-routes.cjs
@@ -1,6 +1,7 @@
 const path=require('path');
 const fs=require('fs');
 const {spawn}=require('child_process');
+const sharp=require('sharp');
 
 module.exports=function registerDailyRoutes(app){
  const ROOT=path.resolve(process.cwd());
